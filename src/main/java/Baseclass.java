62c2398b4f26280af95f3c076a627ad54df8b735
TestVagrant <testvagrant@Siva.local>
2023-12-06 12:35:59 +0530

    Second commit

--- src/main/java/Baseclass.java
+++ src/main/java/Baseclass.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -7,12 +8,14 @@ import java.time.Duration;
 
 public class Baseclass {
     protected WebDriver driver;
+    protected WebDriverWait wait;
     @BeforeMethod
     public void launch(){
-        WebDriver driver=new ChromeDriver();
+         driver=new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("https://web-playground.ultralesson.com/");
+         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
     @AfterMethod
     public void close(){
--- src/test/java/ValidateCartCount.java
+++ src/test/java/ValidateCartCount.java
@@ -4,18 +4,15 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
 
-public class ValidateCartCount {
+public class ValidateCartCount extends Baseclass {
     @Test
-    public void verifyTheCartDeatials() {
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.get("https://web-playground.ultralesson.com/");
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    public void verifyTheCartDeatials() throws InterruptedException {
+
         WebElement Productelements = driver.findElement(By.xpath("//a[contains(.,'15mm Combo Wrench')]"));
         wait.until(ExpectedConditions.visibilityOf(Productelements));
         Productelements.click();
@@ -24,8 +21,27 @@ public class ValidateCartCount {
         if (soldoutElement.contains("Sold out")) {
             System.out.println("This product is Sold out");
         } else {
+            wait.until(ExpectedConditions.elementToBeClickable(element));
             element.click();
-            driver.findElement(By.xpath("//a[@id='cart-icon-bubble']")).click();
+           try{
+               wait.until(ExpectedConditions.alertIsPresent());
+           }catch (Exception e){
+               e.printStackTrace();
+           }
+            WebElement carticon = driver.findElement(By.xpath("//a[@id='cart-icon-bubble']"));
+            wait.until(ExpectedConditions.elementToBeClickable(carticon));
+            carticon.click();
+            String ExpectPrice = driver.findElement(By.xpath("//td[@class='cart-item__totals right small-hide']/descendant::span")).getText();
+            String ActualPrice=driver.findElement(By.xpath("//h3[text()='Subtotal']/following-sibling::p")).getText();
+            String DiscountPrice=driver.findElement(By.xpath("//li[@class='discounts__discount discounts__discount--end']")).getText();
+            String[] exp = ExpectPrice.split(" ");
+            String[] act = ActualPrice.split(" ");
+            double expnu = Double.parseDouble(exp[1]);
+            double actnu = Double.parseDouble(act[1]);
+            double discount = expnu - actnu;
+            System.out.println(discount);
+            Assert.assertTrue(DiscountPrice.contains(DiscountPrice+""));
         }
+        Thread.sleep(3000);
     }
 }
