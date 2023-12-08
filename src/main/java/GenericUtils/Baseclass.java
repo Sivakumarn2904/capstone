2646d4d694fc34819a7109e8fab08e098177aabf
TestVagrant <testvagrant@Siva.local>
2023-12-08 11:35:31 +0530

    10 commit

--- src/main/java/GenericUtils/Baseclass.java
+++ src/main/java/GenericUtils/Baseclass.java
@@ -2,6 +2,8 @@ package GenericUtils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -11,9 +13,34 @@ import java.time.Duration;
 public class Baseclass {
     protected WebDriver driver;
     protected WebDriverWait wait;
+
+    public WebDriver selectBrowser(String browser)
+    {
+        switch (browser) {
+            case "chrome":
+                driver=new ChromeDriver();
+                break;
+            case "firefox":
+                driver=new FirefoxDriver();
+                break;
+            case "edge":
+                driver=new InternetExplorerDriver();
+                break;
+
+            default:
+                System.out.println("Please enter the browser");
+                break;
+        }
+        return driver;
+    }
+    public WebDriver closeBrowser(){
+        driver.quit();
+        return driver;
+    }
     @BeforeMethod
     public void launch(){
-         driver=new ChromeDriver();
+        driver=selectBrowser("chrome");
+//         driver=new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("https://web-playground.ultralesson.com/");
@@ -21,6 +48,7 @@ public class Baseclass {
     }
     @AfterMethod
     public void close(){
-        driver.quit();
+        closeBrowser();
+        //driver.quit();
     }
 }
